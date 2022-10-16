aac0644ace16fac3d452f30791aa5cbc7a9519a1
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-16 18:25:46 +0600

    added useState in UserContext

--- auth-router-context-app/src/contexts/UserContext.js
+++ auth-router-context-app/src/contexts/UserContext.js
@@ -1,9 +1,10 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const AuthContext = createContext();
 
 const UserContext = ({ children }) => {
-  const user = { displayName: "Rimon" };
+  const [user, setUser] = useState({ displayName: "Rimon" });
+  // const user = { displayName: "Rimon" };
   const authInfo = { user: user };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
