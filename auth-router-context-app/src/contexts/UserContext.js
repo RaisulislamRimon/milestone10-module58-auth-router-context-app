dd28f86f020397c2a388266d7f2f34c353afa571
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-16 18:29:20 +0600

    accessing context value from anywhere

--- auth-router-context-app/src/contexts/UserContext.js
+++ auth-router-context-app/src/contexts/UserContext.js
@@ -1,6 +1,10 @@
 import React, { createContext, useState } from "react";
+import app from "../firebaseConfig/firebaseConfig";
+import { getAuth } from "firebase/auth";
 
 export const AuthContext = createContext();
+
+const auth = getAuth(app);
 
 const UserContext = ({ children }) => {
   const [user, setUser] = useState({ displayName: "Rimon" });
