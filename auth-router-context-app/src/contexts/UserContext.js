68811995c8cd284009259209b1f1c25126ff754a
RaisulislamRimon <rimonppip0@gmail.com>
2022-10-16 22:30:28 +0600

    added loading

--- auth-router-context-app/src/contexts/UserContext.js
+++ auth-router-context-app/src/contexts/UserContext.js
@@ -15,7 +15,8 @@ export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const UserContext = ({ children }) => {
-  const [user, setUser] = useState({ displayName: "Rimon" });
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
 
   const googleProvider = new GoogleAuthProvider();
 
@@ -30,6 +31,7 @@ const UserContext = ({ children }) => {
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+      setLoading(false);
       console.log("auth user changed", currentUser);
     });
     return () => {
@@ -45,7 +47,14 @@ const UserContext = ({ children }) => {
     return signOut(auth);
   };
 
-  const authInfo = { user, createUser, signIn, logOut, signInWithGoogle };
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    signIn,
+    logOut,
+    signInWithGoogle,
+  };
 
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
